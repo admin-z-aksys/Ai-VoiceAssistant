f8a11d158937788ee3b816350cf1635eedb35ba6
admin-z-aksys <admin@z-aksys.com>
2025-10-09 12:58:57 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -475,9 +475,9 @@ from pydub import AudioSegment
 from faster_whisper import WhisperModel
 from dotenv import load_dotenv
 
-from config import Config
-from rag import retrieve_context
-from tts import synthesize_with_phonemes
+from .config import Config
+from .rag import retrieve_context
+from .tts import synthesize_with_phonemes
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
